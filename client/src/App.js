f14b9a1a3572cd407521a4874304a22f6bddc75f
JBrickhouse <jbrickhouse777@gmail.com>
2021-02-01 14:11:23 -0600

    Successfully importing the test letters for use in building the app functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,25 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Books from "./pages/Books";
-import Detail from "./pages/Detail";
-import NoMatch from "./pages/NoMatch";
-import Nav from "./components/Nav";
+
+
+
+// import Books from "./pages/Books";
+// import Detail from "./pages/Detail";
+// import NoMatch from "./pages/NoMatch";
+// import Nav from "./components/Nav";
+
+import Home from "./pages/Home"
+
+
+
+
+
 
 function App() {
   return (
     <Router>
       <div>
-        <Nav />
-        <Switch>
-          <Route exact path={["/", "/books"]}>
-            <Books />
-          </Route>
-          <Route exact path="/books/:id">
-            <Detail />
-          </Route>
-          <Route>
-            <NoMatch />
-          </Route>
-        </Switch>
+        <Home/>
       </div>
     </Router>
   );
---
+++ client/src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import word from "../wordData/initial-word-test"
+
+console.log(word)
+
+const Home = () => {
+    return (
+        <div>
+            <p>Home</p>
+        </div>
+    );
+}
+ 
+export default Home;
